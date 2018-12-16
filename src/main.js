f2819ec2609617f64cc1cb1f3416581fc908751c
haitaodesign <15393243613@163.com>
2018-12-16 12:13:15 +0800

    add vue-axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store/index.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import 'normalize.css'
 import { Button } from 'vant'
 Vue.use(Button)
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false
 new Vue({
   router,
